719ca52d7c8e8bc201cfe9cd0912129e066bda77
Mike Rightmire <Mike.Rightmire@BiocomSoftware.com>
2017-10-06 23:24:41 +0200

    2017-10-6 MOR - Updated the unicode for Py3 in RunSubprocess

--- runsubprocess.py
+++ runsubprocess.py
@@ -22,9 +22,10 @@ def RunSubprocess(command, *args, **kwargs):
     """
     """
     verbose = kwargs.pop('verbose',False)
-#     output  = kwargs.pop('output', "string") # Depricated
+    if verbose: verbose = True # Ensure is boolean
     shell   = kwargs.pop('shell', False)
     if shell: shell = True # Ensure is boolean
+    output  = str(kwargs.pop('output', "str"))
     
     if not checks.checkSanitized(command):
         err = ''.join(["RunSubprocess: The command list '", str(command), "' does not pass a sanitization check. !!MAY HAVE DANGEROUS CONTENT!! ABORTING."])
@@ -32,19 +33,6 @@ def RunSubprocess(command, *args, **kwargs):
 
     result = ""
     
-    #=== DEPRICATED ============================================================
-    # if   isinstance(output,list):
-    #     err  
-    #     result = output
-    # elif isinstance(output,str): 
-    #     if re.match('^\s*st.*$', str(output), re.IGNORECASE):       result = str()
-    #     elif re.match('^\s*te?x?.*$', str(output), re.IGNORECASE):  result = str()
-    #     elif re.match('^\s*li?s?t?.*$', str(output), re.IGNORECASE):result = []
-
-    # if result is False: 
-    #    err = ''.join(["BiocomCommon.RunSubprocess: ", "Unable to determine output type. ", "Parameter 'output' must be a Python object of 'str()' or 'list()' or the text 'string' or 'list'. "])
-    #    raise ValueError(err)
-    #===========================================================================
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=shell)
     log.debug("RunSubprocess: " + str(command))
 
@@ -55,23 +43,25 @@ def RunSubprocess(command, *args, **kwargs):
         # Store the output
         else:
             # Strip all the weird control chars
-            _stdout = "".join(ch for ch in unicode(_stdout) if unicodedata.category(ch)[0]!="C")
+            _stdout = "".join(ch for ch in str(_stdout,'utf-8') if unicodedata.category(ch)[0]!="C")
             if len(_stdout) > 0: result += (_stdout + "\n")
-            #=== DEPRICATED ====================================================
-            # elif isinstance(result, list) and len(_stdout) > 0: result.append(_stdout)  
-            #===================================================================
             if verbose is True and len(_stdout) > 1: log.debug("RunSubprocess:" + str(_stdout))
-
-    return result
+    
+    if "str"  in output.lower(): return result # Str is default
+    if "list" in output.lower(): return result.split() 
 
 if __name__ == '__main__':
-    log.debug("__main__", logfile = 'syslog', app_name = "Protein_LFQ_Seg1", log_level = 10, screendump = True)
+#     log.debug("__main__", logfile = 'syslog', app_name = "Protein_LFQ_Seg1", log_level = 10, screendump = True)
     command =["ls", "'-la", "/"
 #               "find", "/usr/", "-iname",  "'*.py'",  
 #                 "|", "grep", "-i", "'python'", "|", "grep", "-i", "'2.7'"
                 ]
     result = RunSubprocess(command, shell = True)
-
+    print(type(result))
+    print(result)
+    l = result.split()
+    print (l)
+    
 
 
 
