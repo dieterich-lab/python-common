4e5f9a902d2a62aad438d6e5a9eb02d4bae99cf3
Mike Rightmire <Mike.Rightmire@BiocomSoftware.com>
2017-10-13 10:13:12 +0200

    2017-10-13 MPR - Added readLineGroup to directory_tools

--- directory_tools.py
+++ directory_tools.py
@@ -41,6 +41,43 @@ def findLinks(dir, use = "default"):
     elif "os" in str(use).lower(): 
         return _findLinks_os(dir)
 
+def readLineGroup(file, N = 1, open_as = "a"):
+    """
+    :NAME:
+        readLineGroup(file, [N = 1])
+    
+    :DESCRIPTION:
+        Reads a file in groups of lines for parsing.
+         
+    :USAGE:
+        readLineGroup(file, [N = 1, open_as = "r"])
+        
+        file: The name fo the file to be opened. 
+        
+        N:    The number of lines to read as a group.
+              DEFAULT = 1
+        
+        open_as: 
+              How to open the file for reading Any of the Python supported 
+              read types are supported, however writing is not allowed (even
+              if opened with "w+".
+              
+    :RETURNS:
+        Yields the read lines as a list.
+        
+    """
+    from itertools import zip_longest
+    
+    def grouper(iterable, n, fillvalue=None):
+        args = [iter(iterable)] * n
+        return zip_longest(*args, fillvalue=fillvalue)
+            
+#     N = 7 # Number of lines to read 
+    with open(file) as f:
+        for lines in grouper(f, N, ''):
+            assert len(lines) == N
+            yield lines
+                
 def _findLinks_python(dir):
     """"""
     result = []
@@ -82,5 +119,12 @@ def _findLinks_os(dir):
         raise NotImplementedError(err) 
 
 if __name__ == '__main__':
-    for i,j in _findLinks_os("//Users/mikes/Documents/tmp"):
-        print (i, ":", j)
+#     for i,j in _findLinks_os("//Users/mikes/Documents/tmp"):
+#         print (i, ":", j)
+
+    for l in readLineGroup("/etc/hosts", N = 2):
+        print("===")
+        print(l)
+        for i in l: print(i)  
+    
+    
