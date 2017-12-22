6df6e6e0533a45777d2f1ed0334e49d049057ca5
Mike Rightmire <Mike.Rightmire@BiocomSoftware.com>
2017-12-22 15:38:06 +0100

    2017-12-22 Merry Christmas!

--- CommandLineClass_template.py
+++ CommandLineClass_template.py
@@ -17,13 +17,20 @@ from argparse       import ArgumentParser
 from common.checks         import Checks
 checks = Checks()
 _delim = checks.directory_deliminator()
+from common.loghandler import log
+from inspect import stack
 
+import abc
 import inspect
+import ntpath
+import os
 
-class ClassName(object):
+class SuperClassName(metaclass=abc.ABCMeta):
     def __init__(self, parser = {}, *args, **kwargs):
+        self.parser = parser
+        self.args   = args
+        self.kwargs = kwargs         
         self._set_config(parser, args, kwargs) # NEVER REMOVE
-        self.main()
                 
     def _arg_parser(self, parser):
         """
@@ -42,7 +49,17 @@ class ClassName(object):
             Returns the parser object for later use by argparse
             
         """
-        parser.add_argument('--root', '-r', action="store", dest="START_DIR", type=str, default = None, help='Starting directory for search.')
+        ### ALWAYS SET DEFAULTS IN @property ##################################
+        parser.add_argument('--someparam', '-P', action="store", dest="SOMEPARAM", type=str, default = None, 
+                            help='Some initial parameters.')
+        parser.add_argument('--logfile', '-L', action="store", dest="LOGFILE", type=str, 
+                            help='Logfile file name or full path.\nDEFAULT: ./classname.log')
+        parser.add_argument('--log-level', '-l', action="store", dest="LOGLEVEL", type=str, 
+                            help='Logging level.\nDEFAULT: 10.')
+        parser.add_argument('--screendump', '-S', action="store", dest="SCREENDUMP", type=str,  
+                            help='For logging only. If "True" all logging info will also be dumped to the terminal.\nDEFAULT: True.')
+        parser.add_argument('--create-paths', '-C', action="store", dest="CREATEPATHS", type=str, 
+                            help='For logging only. If "True" will create all paths and files (example create a non-existent logfile.\nDEFAULT: True')
         return parser
     
     def _set_config(self, parser, args, kwargs):
@@ -68,62 +85,152 @@ class ClassName(object):
         # #=== loghandler disabled until bugfix in Jessie access to self.socket.send(msg)
         # # Here we parse out any args and kwargs that are not needed within the self or self.CONF objects
         # # if "flag" in args: self.flag = something
+        ### ALWAYS SET DEFAULTS IN @property #################################
         # # Logging
-        # self.logfile    = kwargs.pop('log_leveL', 'system') # Default warning
-        # self.log_level  = kwargs.pop('log_leveL', 10) # Default warning
-        # self.screendump = kwargs.pop('screendump', True) # Default off
-        # self.formatter  = kwargs.pop('formatter', '%(asctime)s-%(name)s-%(levelname)s-%(message)s')
-        # self.create_paths = kwargs.pop('create_paths', True) # Automatically create missing paths
-        #=======================================================================
-        # parser stuff
-        self.start_dir = kwargs.pop("START_DIR", '.')
-        
-        # Everything else goes into the conf
-        for key, value in kwargs.iteritems():
-            self.CONF.set(key, value)
-        
-        print(self.__dict__)
-        
-        #=== loghandler disabled until bugfix in Jessie access to self.socket.send(msg)
-        # # Log something
-        # log.debug("Running {C}.{M}...".format(C = self.app_name, M = inspect.stack()[0][3]), 
-        #          app_name     = self.app_name,
-        #          logfile      = self.logfile, 
-        #          log_level    = self.log_level, 
-        #          screendump   = self.screendump, 
-        #          create_paths = self.create_paths, 
-        #          )
-        #=======================================================================
+        self.logfile        = kwargs.pop("LOGFILE", ''.join(["./", self.app_name, ".log"]))
+        self.log_level      = kwargs.pop("LOGLEVEL", 10)
+        self.screendump     = kwargs.pop("SCREENDUMP", True)
+        self.create_paths   = kwargs.pop("CREATEPATHS", True)
+        #=== loghandler bugfix in Jessie access to self.socket.send(msg)
+        # Only use actual filesystem file for log for now
+        # Log something
+        log.debug("Starting  {C}...".format(C = self.app_name), 
+                 app_name     = self.app_name,
+                 logfile      = self.logfile, 
+                 log_level    = self.log_level, 
+                 screendump   = self.screendump, 
+                 create_paths = self.create_paths, 
+                 )
+        # Start params here
+        self.someparam      = kwargs.pop("SOMEPARAM", 'MyParam')
+
             
     @property
-    def start_dir(self):
+    def someparam(self):
         try:
-            return self.START_DIR
+            return self.SOMEPARAM
         except (AttributeError, KeyError, ValueError) as e:
             err = "Attribute {A} is not set. ".format(A = str(stack()[0][3]))
-#             log.error() # loghandler disabled until bugfix in Jessie access to self.socket.send(msg)
+            log.error(err)
             raise ValueError(err)
         
-    @start_dir.setter
-    def start_dir(self, value):
+    @someparam.setter
+    def someparam(self, value):
+        if value is None: value = "MyParam"
         _value = str(value)
-        if not os.path.isdir(_value):
-            err = "The value passed in for attribute {A} ({V}) does not appear to be an existing directory.".format(A = str(stack()[0][3]), V = _value)
-#             log.error(err)# loghandler disabled until bugfix in Jessie access to self.socket.send(msg)
+        # Do checks and such here
+        if (not _value):
+            err = "Attribute '{A} = {V}' does not appear to be valid for reason.".format(A = str(stack()[0][3]), V = _value)
+            log.error(err)
             raise ValueError(err)
         else:
-            self.START_DIR = _value
+            self.SOMEPARAM = _value
     
-    @start_dir.deleter
-    def start_dir(self):
-        del self.START_DIR
+    @someparam.deleter
+    def someparam(self):
+        del self.SOMEPARAM
 
+    @property
+    def logfile(self):
+        try: return self.LOGFILE
+        except (AttributeError, KeyError, ValueError) as e:
+            err = "Attribute {A} is not set. ".format(A = str(stack()[0][3]))
+            log.error(err)
+            raise ValueError(err)
+        
+    @logfile.setter
+    def logfile(self, value):
+        if value is None: value = ''.join(["./", self.app_name, ".log"])
+        _value  = str(value)
+        _dir    = ntpath.dirname(_value)
+        _file   = ntpath.basename(_value)
+        _basefilename, _ext = os.path.splitext(_file)
+        # Do checks and such here
+        if (_dir == "") or (_dir.startswith(".")): _dir = os.getcwd() + _delim
+        _value = _dir + _file 
+        self.LOGFILE = _value
+    
+    @logfile.deleter
+    def logfile(self):
+        del self.LOGFILE
+
+    @property
+    def log_level(self):
+        try: return self.LOGLEVEL
+        except (AttributeError, KeyError, ValueError) as e:
+            err = "Attribute {A} is not set. ".format(A = str(stack()[0][3]))
+            log.error(err)
+            raise ValueError(err)
+        
+    @log_level.setter
+    def log_level(self, value):
+        if value is None: value = 10
+        try: self.LOGLEVEL = int(value)
+        except (ValueError, TypeError):
+            _value = str(value).upper()
+            if   "CRIT"   in _value: self.LOGLEVEL = 50
+            elif "ERR"    in _value: self.LOGLEVEL = 40
+            elif "WARN"   in _value: self.LOGLEVEL = 30
+            elif "INF"    in _value: self.LOGLEVEL = 20
+            elif "D"      in _value: self.LOGLEVEL = 10
+            elif "N"      in _value: self.LOGLEVEL = 0
+            else:
+                err = "Unable to determine log level value from'{V}'".format(str(value))
+                raise ValueError(err)
+                    
+    @log_level.deleter
+    def log_level(self):
+        del self.LOGLEVEL
+
+    @property
+    def screendump(self):
+        try: return self.SCREENDUMP
+        except (AttributeError, KeyError, ValueError) as e:
+            err = "Attribute {A} is not set. ".format(A = str(stack()[0][3]))
+            log.error(err)
+            raise ValueError(err)
+        
+    @screendump.setter
+    def screendump(self, value):
+        if value is None: value = None
+        if value:   self.SCREENDUMP = True
+        else:       self.SCREENDUMP = False
+                    
+    @screendump.deleter
+    def screendump(self):
+        del self.SCREENDUMP
+
+    @property
+    def create_paths(self):
+        try: return self.CREATEPATHS
+        except (AttributeError, KeyError, ValueError) as e:
+            err = "Attribute {A} is not set. ".format(A = str(stack()[0][3]))
+            log.error(err)
+            raise ValueError(err)
+        
+    @create_paths.setter
+    def create_paths(self, value):
+        if value is None: value = True
+        if value:   self.CREATEPATHS = True
+        else:       self.CREATEPATHS = False
+                    
+    @create_paths.deleter
+    def create_paths(self):
+        del self.SCREENDUMP
+    
+    @abstractmethod
     def main(self):
         """"""
-        # loghandler disabled until bugfix in Jessie access to self.socket.send(msg)
-#         log.debug("Done.")
-        pass
+        raise NotImplementedError()
         
+
+class ClassName(SuperClassName):
+    def __init__(self, parser = {}, *args, **kwargs):
+        super().__init__(parser, args, kwargs)
+        self.main()
+        
+    def main(self):
+        pass
     
 if __name__ == '__main__':
     parser = ArgumentParser()
